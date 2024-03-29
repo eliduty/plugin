c98d8363423d9961845be0585d06c0d9134f7038
eliduty <eliduty@qq.com>
2024-03-29 13:43:07 +0800

    feat: 新增vite-plugin-config-html

---
+++ packages/vite-plugin-config-html/src/index.ts
@@ -0,0 +1,114 @@
+import type { HtmlTagDescriptor, Plugin } from 'vite';
+export interface IHTMLTag {
+  [key: string]: string | boolean;
+}
+HTMLScriptElement;
+export type ScriptTag = Record<string, string | boolean> | string;
+export interface Options {
+  favicon: string;
+  title: string;
+  metas: IHTMLTag[];
+  links: IHTMLTag[];
+  style: string;
+  headScripts: ScriptTag[];
+  scripts: ScriptTag[];
+  preHeadScripts: ScriptTag[];
+}
+export default function HtmlPlugin(rawOptions: Partial<Options>): Plugin {
+  const { favicon, title, headScripts = [], metas = [], links = [], style, scripts = [], preHeadScripts = [] } = rawOptions;
+
+  const getScriptContent = (script: ScriptTag, injectTo: HtmlTagDescriptor['injectTo']) => {
+    let result = {} as HtmlTagDescriptor;
+    if (typeof script === 'object' && script.src) {
+      result = {
+        tag: 'script',
+        injectTo,
+        attrs: { ...script }
+      };
+    } else if (typeof script === 'object' && script.content) {
+      const { content, ...attr } = script;
+      result = {
+        tag: 'script',
+        injectTo,
+        attrs: { ...attr },
+        children: `${content}`
+      };
+    } else {
+      result = {
+        tag: 'script',
+        injectTo,
+        children: `${script}`
+      };
+    }
+    return result;
+  };
+
+  return {
+    name: 'html-plugin',
+    transformIndexHtml: {
+      order: 'pre',
+      handler: (html: string) => {
+        let resultHtmlStr = html;
+        const htmlResult = [] as HtmlTagDescriptor[];
+        if (favicon) {
+          htmlResult.push({
+            tag: 'link',
+            attrs: { rel: 'shortcut icon', type: 'image/x-icon', href: favicon },
+            injectTo: 'head'
+          });
+        }
+        if (metas.length) {
+          metas.forEach(meta => {
+            htmlResult.push({
+              tag: 'meta',
+              injectTo: 'head',
+              attrs: { ...meta }
+            });
+          });
+        }
+        if (links.length) {
+          links.forEach(meta => {
+            htmlResult.push({
+              tag: 'link',
+              injectTo: 'head',
+              attrs: { ...meta }
+            });
+          });
+        }
+        if (style && style.length) {
+          htmlResult.push({
+            tag: 'style',
+            injectTo: 'head',
+            children: `${style}`
+              .split('\n')
+              .map(line => `  ${line}`)
+              .join('\n')
+          });
+        }
+        if (title && title.length) {
+          // 如果 title 原本就存在
+          resultHtmlStr = html.replace(/<title>(.*?)<\/title>/, `<title>${title}</title>`);
+        }
+        if (headScripts.length) {
+          headScripts.forEach(script => {
+            htmlResult.push(getScriptContent(script, 'head'));
+          });
+        }
+        if (scripts.length) {
+          scripts.forEach(script => {
+            htmlResult.push(getScriptContent(script, 'body'));
+          });
+        }
+        if (preHeadScripts.length) {
+          preHeadScripts.forEach(script => {
+            htmlResult.push(getScriptContent(script, 'head-prepend'));
+          });
+        }
+        return {
+          html: resultHtmlStr,
+          tags: htmlResult
+        };
+      }
+    }
+  } as Plugin;
+}
